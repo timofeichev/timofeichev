161f0a46d378531719dd513484a35917f3e98b55
timofeichev <a.timofeichev@yandex.ru>
2019-09-11 16:16:04 +0400

    new_zadaniya

--- new_practice.py
+++ new_practice.py
@@ -61,3 +61,56 @@ def my_func_zip(my_list):
     return result
 print(my_func_zip(my_list))
 
+
+#Написать собственную реализацию xrange().
+
+def my_range(stop, start=None, step=None):
+    result = []
+    if stop and start == None and step == None:
+        c = 0
+        while c < stop:
+            result.append(c)
+            c += 1
+
+    elif (start or start == 0) and stop and step == None:
+        while start < stop:
+            result.append(start)
+            start += 1
+
+    elif (start or start == 0) and stop and step:
+        while start < stop:
+            result.append(start)
+            start += step
+    my_generator_object = (elem for elem in result)
+    return my_generator_object
+
+print(my_range(12, start=1, step=2))
+
+
+#Написать «вечный» генератор, который выдаёт всё время одно значение;
+
+# var 1
+def my_gen(n):
+    c = 0
+    while n > c:
+        yield n
+
+print(my_gen(5))
+f = my_gen(5)
+print(next(f))
+print(next(f))
+print(next(f))
+
+# var 2
+from itertools import cycle
+
+def my_generator(n):
+    n = str(n)
+    for i in cycle(n):
+        yield i
+
+print(my_generator(5))
+f = my_generator(5)
+print(next(f))
+print(next(f))
+print(next(f))
